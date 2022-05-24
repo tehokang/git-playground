fa4766176dacf2e1d417591bfecdf0faa4b767d3
teho <tehokoang@gmail.com>
2022-05-24 23:08:33 +0900

    Added moditication 2 for feature/teho/test1

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@ int main(int argc, char **argv)
 {
 	printf("Hello World!\n");
 
-	// added modifications for feature/teho/test1
+	// added modifications 1 for feature/teho/test1
+
+	// added modifications 2 for feature/teho/test1
 
 	return 0;
 }
