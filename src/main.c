49203390ad7844d703277e3378e80ac5d87b70a6
teho <tehokoang@gmail.com>
2022-05-24 23:07:34 +0900

    Added moditication for feature/teho/test1

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(int argc, char **argv) {
-  printf("Hello World!\n");
-  return 0;
+int main(int argc, char **argv)
+{
+	printf("Hello World!\n");
+
+	// added modifications for feature/teho/test1
+
+	return 0;
 }
